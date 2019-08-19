fda07e22f9e2eb4aa451de0f2887a2c5a82441b8
MohammedMuslehuddin7 <54193698+MohammedMuslehuddin7@users.noreply.github.com>
2019-08-18 21:26:28 -0500

    first commit

---
+++ EmployeeWAPI/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace EmployeeWAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ EmployeeWAPI/Areas/HelpPage/ModelDescriptions/EnumValueDescription.cs
@@ -0,0 +1,11 @@
+namespace EmployeeWAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class EnumValueDescription
+    {
+        public string Documentation { get; set; }
+
+        public string Name { get; set; }
+
+        public string Value { get; set; }
+    }
+}
